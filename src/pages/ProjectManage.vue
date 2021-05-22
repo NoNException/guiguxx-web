2ef3a59ce636c7361bf036fe701861882982c35b
zongzi <545670755@qq.com>
2021-05-22 11:49:53 +0800

    添加了滑出的抽屉添加项目

--- src/pages/ProjectManage.vue
+++ src/pages/ProjectManage.vue
@@ -1,7 +1,7 @@
 <template>
   <Row :gutter="16">
     <Col span="5">
-      <Input v-model="value" placeholder="请输入...">
+      <Input placeholder="请输入...">
         <Button slot="append" icon="ios-search" />
       </Input>
       <br />
@@ -15,25 +15,35 @@
     </Col>
     <Col span="19">
       <Row>
+        <Col span="4" offset="20">
+          <Button type="info" size="small" @click="addProject = true">添加</Button>
+          <Drawer title="新增项目" :closable="false" v-model="addProject">
+             aaaaaaaa 
+          </Drawer>
+          <Button type="info" size="small">删除</Button>
+        </Col>
+      </Row>
+      <hr />
+      <Row>
         <Row>项目配置</Row>
         <Row>
           <Col span="4">项目名称</Col>
           <Col>weidian-shop</Col>
         </Row>
         <Row>
-        <Col span="4">代码仓库</Col>
-        <Col span="4">xxxxxxxxx</Col>
-        <Col span="4">代码路径</Col>
-        <Col span="4">xxxxxxxxxxxx</Col>
+          <Col span="4">代码仓库</Col>
+          <Col span="4">xxxxxxxxx</Col>
+          <Col span="4">代码路径</Col>
+          <Col span="4">xxxxxxxxxxxx</Col>
         </Row>
       </Row>
-          <hr />
+      <br>
       <Row>
         <Row>服务配置</Row>
         <Row>
           <Row> Dubbo配置 </Row>
           <Row>
-            <Col span="4"> GroupName </Col>
+            <Col span="4">GroupName</Col>
             <Col span="4">xxxx </Col>
             <Col span="4">dubbo端口</Col>
             <Col span="4">xxxx </Col>
@@ -46,5 +56,11 @@
   </Row>
 </template>
 <script>
-export default {};
+export default {
+    data () {
+            return {
+                addProject: false
+            }
+        }
+};
 </script>
