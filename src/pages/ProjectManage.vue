8e438ca00e7e745828bd3aa8821f184769b51ff5
zongzi <545670755@qq.com>
2021-05-24 14:36:07 +0800

    添加了项目管理的操作组件

--- src/pages/ProjectManage.vue
+++ src/pages/ProjectManage.vue
@@ -1,66 +1,126 @@
 <template>
   <Row :gutter="16">
     <Col span="5">
-      <Input placeholder="请输入...">
-        <Button slot="append" icon="ios-search" />
-      </Input>
-      <br />
-      <ul class="list-group">
-        <li class="list-group-item">weidian-shop</li>
-        <li class="list-group-item">weidian-cms</li>
-        <li class="list-group-item">weidian-psp</li>
-        <li class="list-group-item">weidiango-pps</li>
-        <li class="list-group-item">weidiango-order</li>
-      </ul>
+    <Input placeholder="请输入...">
+    <Button slot="append" icon="ios-search" />
+    </Input>
+    <br />
+    <ul class="list-group">
+      <li class="list-group-item">weidian-shop</li>
+      <li class="list-group-item">weidian-cms</li>
+      <li class="list-group-item">weidian-psp</li>
+      <li class="list-group-item">weidiango-pps</li>
+      <li class="list-group-item">weidiango-order</li>
+    </ul>
     </Col>
     <Col span="19">
+    <Row>
+      <Col span="4" offset="20">
+      <Button type="info" size="small" @click="addProject = true">新增</Button>
+      <Button type="info" size="small" @click="deleteProject = true">删除</Button>
+      <Drawer title="新增项目" :closable="false" v-model="addProject" width='25'>
+        <ProjectModify> </ProjectModify>
+      </Drawer>
+      </Col>
+    </Row>
+    <hr />
+    <Row>
       <Row>
-        <Col span="4" offset="20">
-          <Button type="info" size="small" @click="addProject = true">添加</Button>
-          <Drawer title="新增项目" :closable="false" v-model="addProject">
-             aaaaaaaa 
-          </Drawer>
-          <Button type="info" size="small">删除</Button>
-        </Col>
+        <Col span="4">项目配置</Col>
+        <Col span="4" offset="16"> <Button type="info" size="small">修改配置</Button></Col>
       </Row>
-      <hr />
       <Row>
-        <Row>项目配置</Row>
-        <Row>
-          <Col span="4">项目名称</Col>
-          <Col>weidian-shop</Col>
-        </Row>
-        <Row>
-          <Col span="4">代码仓库</Col>
-          <Col span="4">xxxxxxxxx</Col>
-          <Col span="4">代码路径</Col>
-          <Col span="4">xxxxxxxxxxxx</Col>
-        </Row>
+        <Col span="4">项目名称</Col>
+        <Col>weidian-shop</Col>
       </Row>
-      <br>
       <Row>
-        <Row>服务配置</Row>
+        <Col span="4">代码仓库</Col>
+        <Col span="4">xxxxxxxxx</Col>
+        <Col span="4">代码路径</Col>
+        <Col span="4">xxxxxxxxxxxx</Col>
+      </Row>
+    </Row>
+    <hr />
+    <Row>
+      <Row>服务配置</Row>
+      <Row>
+        <Row>Dubbo配置</Row>
         <Row>
-          <Row> Dubbo配置 </Row>
+          <Col span="4">GroupName</Col>
+          <Col span="4">xxxx </Col>
+          <Col span="4">dubbo端口</Col>
+          <Col span="4">xxxx </Col>
+          <Col span="4">jsonrpc端口</Col>
+          <Col span="4">xxxx </Col>
+        </Row>
+        <br />
+        <Row>
+          <Row>ZK配置</Row>
           <Row>
-            <Col span="4">GroupName</Col>
-            <Col span="4">xxxx </Col>
-            <Col span="4">dubbo端口</Col>
-            <Col span="4">xxxx </Col>
-            <Col span="4">jsonrpc端口</Col>
-            <Col span="4">xxxx </Col>
+            <Col span="4">IP地址</Col>
+            <Col span="4">xxxxxxxxxx</Col>
           </Row>
         </Row>
+        <hr>
+        <Row>
+          <Col span="4"> 服务器配置 </Col>
+          <Col span="4" offset="16">
+          <Button type="info" size="small" @click="addMachine = true">新增服务器</Button>
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Table :columns="columns1" :data="data1">
+            <template slot-scope="{ row, index }" slot="operate">
+              <Button type="primary" size="small" @click="show(index)">Update</Button>
+              <Button type="error" size="small" @click="remove(index)">Delete</Button>
+            </template>
+          </Table>
+        </Row>
       </Row>
+    </Row>
+
     </Col>
   </Row>
 </template>
 <script>
+import ProjectModify from "./ProjectModify"
+
 export default {
-    data () {
-            return {
-                addProject: false
-            }
-        }
+
+  data() {
+    return {
+      addProject: false,
+      deleteProject: false,
+      columns1: [
+        { title: "No.", key: "numbers" },
+        { title: "Ip Address", key: "ipAddress" },
+        { title: "User Name", key: "userName" },
+        { title: "Password", key: "password" },
+        { title: "Tomcat House", key: "tomcatHouse" },
+        { title: "Operation", slot: "operate", align: "center" },
+      ],
+      data1: [
+        {
+          numbers: "weidian_shop_1",
+          ipAddress: "127.0.0.1",
+          userName: "root",
+          password: "xxxx",
+          tomcatHouse: "/root/tomcat/tomcat_shop",
+        },
+        {
+          numbers: "weidian_shop_1",
+          ipAddress: "127.0.0.1",
+          userName: "root",
+          password: "xxxx",
+          tomcatHouse: "/root/tomcat/tomcat_shop",
+        },
+      ],
+      addMachine: false,
+    };
+  },
+  components: {
+    "ProjectModify": ProjectModify
+  }
 };
 </script>
---
+++ src/pages/ProjectModify.vue
@@ -0,0 +1,29 @@
+<template>
+  <Form :model="projectModifyForm" :label-width="100">
+    <FormItem label="项目名称">
+      <Input size="small" />
+    </FormItem>
+    <FormItem label="代码仓库">
+      <Input size="small" />
+    </FormItem>
+    <FormItem label="DubboGroup">
+      <Input size="small" />
+    </FormItem>
+    <FormItem label="Dubbo端口">
+      <Input size="small" />
+    </FormItem>
+    <FormItem label="JsonRpc端口">
+      <Input size="small" />
+    </FormItem>
+    <FormItem label="ZK配置">
+        <Select size="small">
+            <Option value="">192.168.0.1</Option>
+            <Option value="">192.168.0.2</Option>
+        </Select>
+    </FormItem>
+    <FormItem>
+            <Button type="primary">提交</Button>
+            <Button style="margin-left: 8px">取消</Button>
+        </FormItem>
+  </Form>
+</template>
